45d5d78513a1f3a9f814663dc5c98dfe841087f7
Stefan Martinov <stefan.martinov@gmail.com>
2020-04-21 11:52:41 +0200

    feat: new flag to select a single service

--- cmd/actions/install.go
+++ cmd/actions/install.go
@@ -12,35 +12,54 @@ func init() {
 	installCmd.Flags().BoolVarP(&flagUpgrade, "upgrade", "u", false, "Upgrade the service if already installed")
 	installCmd.Flags().StringVarP(&flagOutputDir, "output", "o", "default", "Specifies output directory")
 	installCmd.Flags().StringVarP(&flagNamespace, "namespace", "n", "default", "Specifies the namespace")
+	installCmd.Flags().StringVarP(&flagService, "service", "s", "", "Specifies the service to work with")
 }
 
 var (
 	flagBuild     bool
 	flagUpgrade   bool
 	flagOutputDir string
+	flagService   string
 )
 
 var (
 	installCmd = &cobra.Command{
-		Use:   "install [GROUP] -n {NAMESPACE} -t {TAG}",
+		Use:   "install [GROUP] -n {NAMESPACE} -t {TAG} -s {SERVICE}",
 		Short: "installs a group of services",
 		Long:  "installs a group of services with given namespace and tag version",
 		Args:  cobra.MinimumNArgs(1),
 		Run: func(cmd *cobra.Command, args []string) {
-			_, err := install(args[0], flagNamespace, flagTag, flagDir, flagOutputDir, flagBuild, flagUpgrade, flagVerbose)
+			_, err := install(args[0], flagNamespace, flagTag, flagDir, flagOutputDir, flagService, flagBuild, flagUpgrade, flagVerbose)
 			if err != nil {
-				log.Fatal(err)
+				log.Fatalf("Installation failed with error: %w", err)
 			}
 		},
 	}
 )
 
-func install(group, namespace, tag, workDir, outputDir string, buildService, upgrade, verbose bool) (string, error) {
+func install(group, namespace, tag, workDir, outputDir, service string, buildService, upgrade, verbose bool) (string, error) {
 	cnf := mustNewConfigurd(workDir, tag)
 	sis := cnf.GetServiceItems(namespace, group)
 	if len(sis) == 0 {
 		return "", fmt.Errorf("could not find any service for namespace: %v and group: %v", namespace, group)
 	}
+
+	// If one service is selected
+	if service != "" {
+		filtered := make([]configurd.ServiceItem, 1)
+		for _, si := range sis {
+			if si.Name == service {
+				filtered[0] = si
+				break
+			}
+		}
+		sis = filtered
+	}
+
+	if len(sis) == 0 {
+		return "", fmt.Errorf("no services found to install")
+	}
+
 	if buildService {
 		log.Printf("Building services")
 		for _, si := range sis {
