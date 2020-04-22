67ebc7a3965fcda992784d3ed776a996284f969b
Stefan Martinov <stefan.martinov@gmail.com>
2020-04-22 14:44:02 +0200

    chore: move Service to configurd file

--- build.go
+++
@@ -1,9 +0,0 @@
-package configurd
-
-type Service struct {
-	Name  string `yaml:"-"`
-	Image string `yaml:"image"`
-	Tag   string `yaml:"tag"`
-	Build string `yaml:"build"`
-	Chart string `yaml:"chart"`
-}
--- configurd.go
+++ configurd.go
@@ -55,6 +55,14 @@ type Configurd struct {
 	Namespaces []Namespace
 }
 
+type Service struct {
+	Name  string `yaml:"-"`
+	Image string `yaml:"image"`
+	Tag   string `yaml:"tag"`
+	Build string `yaml:"build"`
+	Chart string `yaml:"chart"`
+}
+
 type serviceLoader func(string) (Service, error)
 
 type Logger interface {
