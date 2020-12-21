553990e776fb4eb66226c84677a6e3ed91934e25
Milos Pejanovic <pejan123@gmail.com>
2020-12-21 11:04:55 +0100

    add gen command to package charts

--- util/helm.go
+++ util/helm.go
@@ -12,12 +12,17 @@ func NewHelmCommand(l *logrus.Entry) *HelmCmd {
 
 func (c HelmCmd) UpdateDependency(chart, chartPath string) (string, error) {
 	c.l.Infof("Running helm dependency update for chart: %v", chart)
-	return c.Args("dependency", "update", chartPath, "--skip-refresh").Run()
+	return c.Base().Args("dependency", "update", chartPath, "--skip-refresh").Run()
 }
 
 func (c HelmCmd) Install(chart, chartPath string) (string, error) {
 	c.l.Infof("Running helm install for chart: %v", chart)
 	return c.Args("upgrade", chart, chartPath, "--install").Run()
+}
+
+func (c HelmCmd) Package(chart, chartPath, destPath string) (string, error) {
+	c.l.Infof("Running helm package for chart: %v", chart)
+	return c.Base().Args("package", chartPath, "--destination", destPath).Run()
 }
 
 func (c HelmCmd) Uninstall(chart string) (string, error) {
